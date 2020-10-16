5e14df6f0d907b4ea21e7531472ed68669f6069c
Angelo Filho <ampsf@icomp.ufam.edu.br>
2020-10-16 13:49:43 -0400

    preset project

---
+++ public/script.js
@@ -0,0 +1,4 @@
+//Babel :Converter(transpilar) código do React para um código que o
+// browser entenda
+//Webpack: pra cada tipo de arquivo(.js .css . png) eu vou converter o código de ma maneira diferente
+//Loader: babel loader, css-loader, image-loader
