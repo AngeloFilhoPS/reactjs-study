cbe286469d4f9e7fe38e68a9faf45e00edcf1861
Angelo <ampsf@icomp.ufam.edu.br>
2020-11-11 16:04:19 -0400

    web pack error

--- public/script.js
+++
@@ -1,4 +0,0 @@
-//Babel :Converter(transpilar) código do React para um código que o
-// browser entenda
-//Webpack: pra cada tipo de arquivo(.js .css . png) eu vou converter o código de ma maneira diferente
-//Loader: babel loader, css-loader, image-loader
